29343a7c52324d2f0e7fbf348f23031edb889157
Abhirag <abhirag@applab.qa>
2025-05-21 11:01:42 +0530

    first commit

---
+++ apps/dcs-fe-dev/src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+// @ts-ignore
+import Header from '@shared/layout/Header';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Header />
+        <h1>Welcome</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ shared/layout/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header style={{ backgroundColor: '#007bff', padding: '10px', color: 'white' }
+        }>
+            <h1>Company Portal</h1>
+        </header>
+    );
+};
+
+export default Header;
