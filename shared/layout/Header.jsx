be17aeea89715e866bd0c612e38fe1cabb71c57f
Abhirag <abhirag@applab.qa>
2025-05-21 11:09:36 +0530

    updates

--- shared/layout/Header.jsx
+++ shared/layout/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <header style={{ backgroundColor: '#007bff', padding: '10px', color: 'white' }
         }>
-            <h1>Company Portal</h1>
+            <h1>Company Portal test data</h1>
         </header>
     );
 };
